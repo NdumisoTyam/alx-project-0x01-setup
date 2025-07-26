f0ae7fcd9e56b84a8e86918b3079b6f4368dfd96
NdumisoTyam <Ndumisotyam1@gmail.com>
2025-07-26 12:22:09 +0200

    Update index.tsx

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,1 +1,33 @@
 import Header from "@/components/layout/Header";
+
+import React from "react";
+import { UserProps } from "../../interfaces";
+import UserCard from "../../components/common/UserCard";
+
+interface UsersPageProps {
+  posts: UserProps[];
+}
+
+const Users: React.FC<UsersPageProps> = ({ posts }) => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Users</h1>
+      {posts.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default Users;
